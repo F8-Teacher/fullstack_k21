b3773a21c2f1425edc2258ab6f682efbd3804c18
Hoang An Unicode <hoangan.web@gmail.com>
2026-08-27 20:38:50 +0700

    Add JavaScript functionality for item manipulation and context menu in DOM exercises

---
+++ 5.javascript/5.15.dom/js/ex10.js
@@ -0,0 +1,100 @@
+const ul = document.querySelector('ul');
+const deleteBtn = document.querySelector('.delete-btn');
+const highlight = (element) => {
+    element.classList.add('highlight');
+    setTimeout(() => {
+        element.classList.remove('highlight');
+    }, 300);
+}
+ul.addEventListener('click', ({ target }) => {
+    const li = target.parentElement;
+    //Down
+    if (target.classList.contains('down')) {
+        const nextElement = li.nextElementSibling;
+        if (!nextElement) {
+            return;
+        }
+        ul.insertBefore(nextElement, li);
+        highlight(li);
+    }
+
+    //Up
+    if (target.classList.contains('up')) {
+        const prevElement = li.previousElementSibling;
+        if (!prevElement) {
+            return;
+        }
+        ul.insertBefore(li, prevElement);
+        highlight(li);
+    }
+
+    if (target.nodeName === 'LI') {
+        const itemSelected = ul.querySelector('.selected');
+        target.classList.toggle('selected');
+        if (itemSelected) {
+            itemSelected.classList.remove('selected');
+        }
+        //Enable delete button
+        deleteBtn.disabled = !target.classList.contains('selected');
+    }
+});
+
+//Context menu
+let currentContext;
+const createItem = (parent, text, listener) => {
+    const item = document.createElement('p');
+    item.className = 'menu-item';
+    item.innerText = text;
+    parent.append(item);
+    if (typeof listener === 'function') {
+        item.addEventListener('click', listener);
+    }
+}
+const createContextMenu = (element, style = {}) => {
+    if (currentContext) {
+        document.body.removeChild(currentContext);
+    }
+    const list = document.createElement('div');
+    list.className = 'context-menu';
+    Object.assign(list.style, style);
+    createItem(list, 'Edit', () => {
+        console.log('Edit');
+        document.body.removeChild(currentContext);
+        currentContext = null;
+    });
+    createItem(list, 'Delete', () => {
+        element.remove();
+        document.body.removeChild(currentContext);
+        currentContext = null;
+    });
+    document.body.append(list);
+    currentContext = list;
+}
+ul.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    const { target } = e;
+    if (target.nodeName === 'LI') {
+        createContextMenu(target, {
+            top: e.clientY + 'px',
+            left: e.clientX + 'px'
+        });
+    }
+});
+
+document.addEventListener('click', (e) => {
+    const itemSelected = ul.querySelector('.selected');
+    if (itemSelected && !ul.contains(e.target)) {
+        itemSelected.classList.remove('selected');
+    }
+
+    if (currentContext && !currentContext.contains(e.target)) {
+        document.body.removeChild(currentContext);
+        currentContext = null;
+    }
+});
+
+deleteBtn.addEventListener('click', () => {
+    const itemSelected = ul.querySelector('.selected');
+    itemSelected.remove();
+    deleteBtn.disabled = true;
+})
